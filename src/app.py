203a27efce4d365565677cfa192144803254b409
Welington Sampaio <welington.sampaio@icloud.com>
2022-09-19 16:44:08 +0000

    :sparkles: add value with extra cmd arg

--- src/app.py
+++ src/app.py
@@ -1,15 +1,14 @@
 from kafka import KafkaConsumer, TopicPartition, OffsetAndMetadata, KafkaProducer
 from datetime import datetime
 import os
+import sys
 
 TOPIC_NAME = os.environ.get('KAFKA_TOPIC', '')
 MESSAGE_KEY = os.environ.get('KAFKA_MESSAGE_KEY', '')
-MESSAGE_VALUE = os.environ.get('KAFKA_MESSAGE_VALUE', '')
+MESSAGE_VALUE = os.environ.get('KAFKA_MESSAGE_VALUE', sys.argv[1])
 BOOSTRAP_SERVER = os.environ.get('KAFKA_BOOSTRAP_SERVER', '')
 
-
-producer = KafkaProducer(bootstrap_servers=BOOSTRAP_SERVER)
-
+producer = KafkaProducer(bootstrap_servers=BOOSTRAP_SERVER.split(","))
 
 print("producing message... ")
 producer.send(TOPIC_NAME, key=MESSAGE_KEY.encode('utf-8'), value=MESSAGE_VALUE.encode('utf-8')).get(timeout=30)
